93c370118540cc36c5cbf3b37b4b0f50b6d71e43
Ben Murray <benmurray573@gmail.com>
2023-01-25 10:20:09 +0000

    formatting

--- src/texas-holdem.test.ts
+++ src/texas-holdem.test.ts
@@ -1,27 +1,45 @@
-import { checkForStraightFlush, hand, removeSuits } from "./texas-holdem"
+import { checkForStraightFlush, hand, removeSuits } from "./texas-holdem";
 
 test("Hand with no winning cards scores *nothing* and ranks with A,K descending...", () => {
-    expect(hand(['K♠', 'A♦'], ['J♣', 'Q♥', '9♥', '2♥', '3♦'])).toEqual({ type: 'nothing', ranks: ['A', 'K', 'Q', 'J', '9'] })
+  expect(hand(["K♠", "A♦"], ["J♣", "Q♥", "9♥", "2♥", "3♦"])).toEqual({
+    type: "nothing",
+    ranks: ["A", "K", "Q", "J", "9"],
+  });
 });
 
 test("Player scores *pair* and ranks should contain four values...", () => {
-    expect(hand(['6♠', 'J♦'], ['J♣', 'K♥', '9♥', '2♥', '3♦'])).toEqual({ type: 'pair', ranks: ['Q', 'K', 'J', '9'] })
+  expect(hand(["6♠", "J♦"], ["J♣", "K♥", "9♥", "2♥", "3♦"])).toEqual({
+    type: "pair",
+    ranks: ["Q", "K", "J", "9"],
+  });
 });
 
 test("Player scores *two pair* and ranks should contain 3 values...", () => {
-    expect(hand(['K♠', 'J♦'], ['J♣', 'K♥', '9♥', '2♥', '3♦'])).toEqual({ type: 'two pair', ranks: ['K', 'J', '9'] })
+  expect(hand(["K♠", "J♦"], ["J♣", "K♥", "9♥", "2♥", "3♦"])).toEqual({
+    type: "two pair",
+    ranks: ["K", "J", "9"],
+  });
 });
 
 test("Player scores *three-of-a-kind* and ranks should contain 3 values...", () => {
-    expect(hand(['4♠', '9♦'], ['J♣', 'Q♥', 'Q♠', '2♥', 'Q♦'])).toEqual({ type: 'three-of-a-kind', ranks: ['Q', 'J', '9'] })
+  expect(hand(["4♠", "9♦"], ["J♣", "Q♥", "Q♠", "2♥", "Q♦"])).toEqual({
+    type: "three-of-a-kind",
+    ranks: ["Q", "J", "9"],
+  });
 });
 
 test("Player scores *straight* and ranks should contain 5 values...", () => {
-    expect(hand(['Q♠', '2♦'], ['J♣', '10♥', '9♥', 'K♥', '3♦'])).toEqual({ type: 'straight', ranks: ['K', 'Q', 'J', '10', '9'] })
+  expect(hand(["Q♠", "2♦"], ["J♣", "10♥", "9♥", "K♥", "3♦"])).toEqual({
+    type: "straight",
+    ranks: ["K", "Q", "J", "10", "9"],
+  });
 });
 
 test("Player scores *flush* and ranks should contain 5 values...", () => {
-    expect(hand(['A♠', 'K♦'], ['J♥', '5♥', '10♥', 'Q♥', '3♥'])).toEqual({ type: 'flush', ranks: ['Q', 'J', '10', '5', '3'] })
+  expect(hand(["A♠", "K♦"], ["J♥", "5♥", "10♥", "Q♥", "3♥"])).toEqual({
+    type: "flush",
+    ranks: ["Q", "J", "10", "5", "3"],
+  });
 });
 
 /*
@@ -30,29 +48,53 @@ flush: J♥, 5♥, 10♥, Q♥, 3♥ -- values dont matter, 5 ♥'s
 */
 
 test("Player scores *full house* and ranks should contain 2 values...", () => {
-    expect(hand(['A♠', 'A♦'], ['K♣', 'K♥', 'A♥', 'Q♥', '3♦'])).toEqual({ type: 'full house', ranks: ['A', 'K'] })
+  expect(hand(["A♠", "A♦"], ["K♣", "K♥", "A♥", "Q♥", "3♦"])).toEqual({
+    type: "full house",
+    ranks: ["A", "K"],
+  });
 });
 
 test("Player scores *four-of-a-kind* and ranks should contain 2 values...", () => {
-    expect(hand(['2♠', '3♦'], ['2♣', '2♥', '3♠', '3♥', '2♦'])).toEqual({ type: 'four-of-a-kind', ranks: ['2', '3'] })
+  expect(hand(["2♠", "3♦"], ["2♣", "2♥", "3♠", "3♥", "2♦"])).toEqual({
+    type: "four-of-a-kind",
+    ranks: ["2", "3"],
+  });
 });
 
 test("Player scores *straight-flush* and ranks should contain 5 values...", () => {
-    expect(hand(['8♠', '6♠'], ['7♠', '5♠', '9♠', 'J♠', '10♠'])).toEqual({ type: 'straight-flush', ranks: ['J', '10', '9', '8', '7'] })
+  expect(hand(["8♠", "6♠"], ["7♠", "5♠", "9♠", "J♠", "10♠"])).toEqual({
+    type: "straight-flush",
+    ranks: ["J", "10", "9", "8", "7"],
+  });
 });
 
 test("removeSuits fn takes an array of cards and removes the suit icons", () => {
-    expect(removeSuits(['7♠', '5♠', '9♠', 'J♠', '10♠'])).toEqual(['7', '5', '9', 'J', '10'])
+  expect(removeSuits(["7♠", "5♠", "9♠", "J♠", "10♠"])).toEqual([
+    "7",
+    "5",
+    "9",
+    "J",
+    "10",
+  ]);
 });
 
 test("checkForStraightFlush fn takes an array of numbers, reverts to array of cards as strings and returns true if flush present", () => {
-    expect(checkForStraightFlush([5, 7, 9, 10, 11], ['8♠', '6♠', '7♠', '5♠', '9♠', 'J♠', '10♠'])).toEqual(true)
+  expect(
+    checkForStraightFlush(
+      [5, 7, 9, 10, 11],
+      ["8♠", "6♠", "7♠", "5♠", "9♠", "J♠", "10♠"]
+    )
+  ).toEqual(true);
 });
 
 test("checkForStraightFlush fn takes an array of numbers, reverts to array of cards as strings and returns false if no flush", () => {
-    expect(checkForStraightFlush([10, 11, 12, 13, 14], ['A♠', 'K♦', 'J♥', '5♥', '10♥', 'Q♥', '3♥'])).toEqual(false)
-});  
-
+  expect(
+    checkForStraightFlush(
+      [10, 11, 12, 13, 14],
+      ["A♠", "K♦", "J♥", "5♥", "10♥", "Q♥", "3♥"]
+    )
+  ).toEqual(false);
+});
 
 // test("", () => {
 //     removeValues([]).toEqual([])
@@ -69,5 +111,3 @@ test("checkForStraightFlush fn takes an array of numbers, reverts to array of ca
 // test("", () => {
 //     countOccurrences([]).toEqual([])
 // })
-
-
--- src/texas-holdem.ts
+++ src/texas-holdem.ts
@@ -1,248 +1,275 @@
 export function hand(holeCards: string[], communityCards: string[]) {
-  const result = { type: '', ranks: [] }
-  let combinedHand: string[] = []
+  const result = { type: "", ranks: [] };
+  let combinedHand: string[] = [];
   for (const card of holeCards) {
-    communityCards.push(card)
+    communityCards.push(card);
   }
-  combinedHand = communityCards
-  console.log("full river + hand: ", combinedHand)
+  combinedHand = communityCards;
+  console.log("full river + hand: ", combinedHand);
 
   //combinedHand now represents the full river plus the two cards in hand
 
   //function to be used for checking pairs/3-of-a-kind/4-of-a-kind
   function countOccurrences(combinedHand: string[], target: string) {
-    let occurrences = 0
+    let occurrences = 0;
     for (const card of combinedHand) {
       if (card.includes(target)) {
-        occurrences += 1
+        occurrences += 1;
       }
     }
-    return occurrences
+    return occurrences;
   }
   //checking for pairs, 3OAK, 4OAK
-  let occurrencesObj: Record<string, number> = {}
+  let occurrencesObj: Record<string, number> = {};
   for (const card of removeSuits(combinedHand)) {
     if (countOccurrences(combinedHand, card) > 1) {
-      occurrencesObj[card] = countOccurrences(combinedHand, card)
+      occurrencesObj[card] = countOccurrences(combinedHand, card);
     }
   }
-  console.log("occurrences: ", occurrencesObj)
+  console.log("occurrences: ", occurrencesObj);
 
   //if nothing
-  if (Object.keys(occurrencesObj).length === 0
-    && checkForStraight(combinedHand).length === 0
-    && checkForFlush(combinedHand) === false) {
-    result.type = 'nothing'
+  if (
+    Object.keys(occurrencesObj).length === 0 &&
+    checkForStraight(combinedHand).length === 0 &&
+    checkForFlush(combinedHand) === false
+  ) {
+    result.type = "nothing";
   }
 
   //if pair...
-  if (Object.keys(occurrencesObj).length === 1 && occurrencesObj[Object.keys(occurrencesObj)[0]] === 2) {
+  if (
+    Object.keys(occurrencesObj).length === 1 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 2
+  ) {
     //if the occurrencesObj obj has only one key/val pair, and its value is 2
-    result.type = 'pair'
+    result.type = "pair";
   }
   //if two pair...
-  if (Object.keys(occurrencesObj).length === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[0]] === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[1]] === 2
+  if (
+    Object.keys(occurrencesObj).length === 2 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 2 &&
+    occurrencesObj[Object.keys(occurrencesObj)[1]] === 2
   ) {
     //if the occurrencesObj obj has two key/val pairs, and both values are 2
-    result.type = 'two pair'
+    result.type = "two pair";
   }
   //if 3OAK...
-  if (Object.keys(occurrencesObj).length === 1
-    && occurrencesObj[Object.keys(occurrencesObj)[0]] === 3) {
+  if (
+    Object.keys(occurrencesObj).length === 1 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 3
+  ) {
     //     && checkForStraight(combinedHand) === false
     //if the occurrencesObj obj has only one key/val pair, and its value is 3
-    result.type = 'three-of-a-kind'
+    result.type = "three-of-a-kind";
   }
   //if straight...
-  if (checkForStraight(combinedHand).length > 1 && checkForFlush(removeValues(combinedHand)) === false) {
-    result.type = 'straight'
+  if (
+    checkForStraight(combinedHand).length > 1 &&
+    checkForFlush(removeValues(combinedHand)) === false
+  ) {
+    result.type = "straight";
   }
 
   //if flush...
-  if (checkForStraight(combinedHand).length === 0
-    && checkForFlush(removeValues(combinedHand)) === true) {
-    result.type = 'flush'
+  if (
+    checkForStraight(combinedHand).length === 0 &&
+    checkForFlush(removeValues(combinedHand)) === true
+  ) {
+    result.type = "flush";
   }
   //if full-house (need to check for 2,3 && 3,2)
-  if (Object.keys(occurrencesObj).length === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[0]] === 3
-    && occurrencesObj[Object.keys(occurrencesObj)[1]] === 2) {
+  if (
+    Object.keys(occurrencesObj).length === 2 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 3 &&
+    occurrencesObj[Object.keys(occurrencesObj)[1]] === 2
+  ) {
     //     && checkForStraight(combinedHand) === false
     //if the occurrencesObj obj has only one key/val pair, and its value is 3
-    result.type = 'full house'
+    result.type = "full house";
   }
-  if (Object.keys(occurrencesObj).length === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[0]] === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[1]] === 3) {
+  if (
+    Object.keys(occurrencesObj).length === 2 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 2 &&
+    occurrencesObj[Object.keys(occurrencesObj)[1]] === 3
+  ) {
     //     && checkForStraight(combinedHand) === false
     //if the occurrencesObj obj has only one key/val pair, and its value is 3
-    result.type = 'full house'
+    result.type = "full house";
   }
 
   //if 4OAK...
-  if (Object.keys(occurrencesObj).length === 1 && occurrencesObj[Object.keys(occurrencesObj)[0]] === 4) {
+  if (
+    Object.keys(occurrencesObj).length === 1 &&
+    occurrencesObj[Object.keys(occurrencesObj)[0]] === 4
+  ) {
     //if the occurrencesObj obj has only one key/val pair, and its value is 4
-    result.type = 'four-of-a-kind'
-  }
-  else if (Object.keys(occurrencesObj).length === 2
-    && occurrencesObj[Object.keys(occurrencesObj)[0]] === 4
-    || occurrencesObj[Object.keys(occurrencesObj)[1]] === 4) {
+    result.type = "four-of-a-kind";
+  } else if (
+    (Object.keys(occurrencesObj).length === 2 &&
+      occurrencesObj[Object.keys(occurrencesObj)[0]] === 4) ||
+    occurrencesObj[Object.keys(occurrencesObj)[1]] === 4
+  ) {
     //if the occurrences obj has only two key/val pairs, and one of the values is 4
-    result.type = 'four-of-a-kind'
+    result.type = "four-of-a-kind";
   }
   //if straight-flush
-  if (checkForStraight(combinedHand).length > 1 && checkForFlush(removeValues(combinedHand))) {
-      result.type = checkForStraightFlush(checkForStraight(combinedHand), combinedHand) === true ? 'straight-flush' : 'flush'
+  if (
+    checkForStraight(combinedHand).length > 1 &&
+    checkForFlush(removeValues(combinedHand))
+  ) {
+    result.type =
+      checkForStraightFlush(checkForStraight(combinedHand), combinedHand) ===
+      true
+        ? "straight-flush"
+        : "flush";
   }
-  return result
+  return result;
   //ranks val can be determined by combining arrays and removing all repeated values (and suits)
   //need to ordered by significance - can be done by value EXCEPT for pair, 3OAK, 4OAK, full-house
   //(player could have an A in hand that does not contribute to win, therefore it would not be their highest ranking card)
-
 
   // function findRanks(removedSuits) {
 
   // }
   //function to be used for checking hand for flush
-  function checkForFlush(removedValues: string[]): boolean { 
+  function checkForFlush(removedValues: string[]): boolean {
     // ['♥', '♠', '♠', '♥', '♠', '♠', '♦']
     // [h, s, d]
-    const uniqueSuits: string[] = []
+    const uniqueSuits: string[] = [];
     for (const suit of removedValues) {
       if (!uniqueSuits.includes(suit)) {
-        uniqueSuits.push(suit)
+        uniqueSuits.push(suit);
       }
     }
     if (uniqueSuits.length > 3) {
-      return false
-    }
-    else return true
+      return false;
+    } else return true;
   }
   //option: declare an array of the possible suits as strings
   //option: loop and count occurrences of any opposing suits, if > 2, is not a flush, otherwise it is
 
   // function to be used for checking hand for Straight combinations
-  function checkForStraight(combinedHand: string[]): number[]{
+  function checkForStraight(combinedHand: string[]): number[] {
     /*
     ['A♠', 'K♦', 'J♥', '5♥', '10♥', 'Q♥', '3♥']
     {A: '♠', K: ....}
     {A: 14, K: 13, ...}
     */
     const values = removeSuits(combinedHand).map((card: string | number) => {
-      return card === 'A' ? card = 14 :
-        card === 'K' ? card = 13 :
-          card === 'Q' ? card = 12 :
-            card === 'J' ? card = 11 :
-              Number(card)
-    })
-    const sortedArr = values.sort((a, b) => a - b)
+      return card === "A"
+        ? (card = 14)
+        : card === "K"
+        ? (card = 13)
+        : card === "Q"
+        ? (card = 12)
+        : card === "J"
+        ? (card = 11)
+        : Number(card);
+    });
+    const sortedArr = values.sort((a, b) => a - b);
     // [ 'J', '10', '9', 'K', '3', 'Q', '2' ] => [ 2, 3, 9, 10, 11, 12, 13 ]
     //full river + hand now sorted by numerical value in asc order
     //checking for any run of five...
     //start at first value - is next value +1? if yes, add one, and continue - if get to 5, return true
-    function checkRun(sortedArr: number[]): number[]{
-      let run = 0
-      let index = 0
-      let currentVal = sortedArr[index]
-      let straightArr = []
+    function checkRun(sortedArr: number[]): number[] {
+      let run = 0;
+      let index = 0;
+      let currentVal = sortedArr[index];
+      let straightArr = [];
       while (run !== 5) {
-        const nextVal = sortedArr[index + 1]
+        const nextVal = sortedArr[index + 1];
         if (currentVal + 1 === nextVal) {
-          currentVal = nextVal
+          currentVal = nextVal;
           if (run === 0) {
-            run = 2
-            straightArr.push(currentVal)
+            run = 2;
+            straightArr.push(currentVal);
+          } else {
+            run++;
+            straightArr.push(currentVal);
           }
-          else {
-            run++
-            straightArr.push(currentVal)
-          }
-          index++
-        }
-        else {
-          run *= 0
-          currentVal = nextVal
-          straightArr = []
-          index++
+          index++;
+        } else {
+          run *= 0;
+          currentVal = nextVal;
+          straightArr = [];
+          index++;
         }
         if (nextVal === undefined) {
-          return []
+          return [];
         }
       }
-      return run === 5 ? straightArr : []
+      return run === 5 ? straightArr : [];
     }
-    console.log(checkRun(sortedArr))
-    return checkRun(sortedArr)
+    console.log(checkRun(sortedArr));
+    return checkRun(sortedArr);
   }
 }
 
 //helper function that removes suits from a combined hand + river
 export function removeSuits(combinedHand: string[]): string[] {
   const removedSuits = combinedHand.map((card) => {
-    return card.length === 2 ? card[0] : card.slice(0, 2)
-  })
-  return removedSuits
+    return card.length === 2 ? card[0] : card.slice(0, 2);
+  });
+  return removedSuits;
 }
 function removeValues(combinedHand: string[]): string[] {
   const removedValues = combinedHand.map((card) => {
-    return card.length === 2 ? card[1] : card[2] //'Q♥'' => '♥', '10♥'' => '♥'
-  })
-  return removedValues
+    return card.length === 2 ? card[1] : card[2]; //'Q♥'' => '♥', '10♥'' => '♥'
+  });
+  return removedValues;
 }
-export function checkForStraightFlush(straightArr: number[], combinedHand: string[]): boolean {
+export function checkForStraightFlush(
+  straightArr: number[],
+  combinedHand: string[]
+): boolean {
   // [10, 11, 12, 13, 14]
   const valuesToCardArr = straightArr.map((value) => {
-    return JSON.stringify(value)
-  })
+    return JSON.stringify(value);
+  });
   // ['10', '11', '12', '13', '14']
   const stringsToCardArr = valuesToCardArr.map((value) => {
-    if(value === '11') {
-      return 'J'
+    if (value === "11") {
+      return "J";
     }
-    if(value === '12') {
-      return 'Q'
+    if (value === "12") {
+      return "Q";
     }
-    if(value === '13') {
-      return 'K'
+    if (value === "13") {
+      return "K";
     }
-    if(value === '14') {
-      return 'A'
-    }
-    else return value
-  })
+    if (value === "14") {
+      return "A";
+    } else return value;
+  });
   // ['10', 'J', 'Q', 'K', 'A']
   //need to get from ^ to => ['10♥', 'J♥', 'Q♥', 'K♦', 'A♠']
 
-    let originalCardsArr = []
-     for(let string of stringsToCardArr) {
-          for(let card of combinedHand) {
-            if(card.includes(string)) {
-             originalCardsArr.push(card)
-            }
-          }
+  let originalCardsArr = [];
+  for (let string of stringsToCardArr) {
+    for (let card of combinedHand) {
+      if (card.includes(string)) {
+        originalCardsArr.push(card);
+      }
     }
-    // ['10♥', 'J♥', 'Q♥', 'K♦', 'A♠']
-    //now check if this straightArr is a flush...?
-    let originalSuits = removeValues(originalCardsArr)
-  const uniqueSuits: string[] = []
+  }
+  // ['10♥', 'J♥', 'Q♥', 'K♦', 'A♠']
+  //now check if this straightArr is a flush...?
+  let originalSuits = removeValues(originalCardsArr);
+  const uniqueSuits: string[] = [];
   for (const suit of originalSuits) {
     if (!uniqueSuits.includes(suit)) {
-      uniqueSuits.push(suit)
+      uniqueSuits.push(suit);
     }
   }
   if (uniqueSuits.length > 1) {
-    return false
-  }
-  else return true
+    return false;
+  } else return true;
 }
 
-
-console.log(hand(['K♠', 'J♦'], ['J♣', 'K♥', '9♥', '2♥', '3♦']))
-console.log(hand(['A♠', 'K♦'], ['J♥', '5♥', '10♥', 'Q♥', '3♥']))
+console.log(hand(["K♠", "J♦"], ["J♣", "K♥", "9♥", "2♥", "3♦"]));
+console.log(hand(["A♠", "K♦"], ["J♥", "5♥", "10♥", "Q♥", "3♥"]));
 // export default hand
-
 
 /*
 
@@ -283,6 +310,3 @@ Straight flush condtions:
 
 ranks - refers to each unique value in the final river after players hand has been inserted
 */
-
-
-
