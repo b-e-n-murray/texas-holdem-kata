e134352b8119ec403a13f3e50b780dc6e5abc637
Ben Murray <benmurray573@gmail.com>
2023-01-26 11:24:47 +0000

    checkRun working, tests passing

--- src/tests/check-for-run.test.ts
+++ src/tests/check-for-run.test.ts
@@ -5,6 +5,11 @@ test(`loops over array of numbers sorted ascendingly,
   that exist in the sequence`, () => {
   expect(checkRun([5, 7, 8, 9, 10, 11, 14])).toEqual([7, 8, 9, 10, 11]);
 });
+test(`loops over array of numbers sorted ascendingly,
+ returns a new array of the 5 numbers
+  that exist in the sequence`, () => {
+  expect(checkRun([2, 3, 5, 6, 7, 8, 9])).toEqual([7, 8, 9, 10, 11]);
+});
 
 test(`loops over array of numbers sorted ascendingly,
  returns an empty array if no run of 5 is found`, () => {
